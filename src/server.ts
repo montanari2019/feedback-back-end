a1502dd57cb157ad42f145c61345f0149686efd3
montanari2019 <ikaro.montanari31@gmail.com>
2022-05-04 21:59:14 -0400

    Feddeback router create ok

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import { prisma } from "./pisma";
 
 const app = express();
 
-const port = 3333
+app.use(express.json());
 
 app.get("/", (req, res) => {
     return res.json("Api rodando")
 })
+
+app.post("/feedbacks", async (req, res) => {
+
+    const { comment, type, screenshot } = req.body
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    })
+
+    return res.status(201).json({ data: feedback })
+})
+
+const port = 3333
+
 
 app.listen(port, () =>{
     console.log(`Servidor rodando na porta ${port}`)
